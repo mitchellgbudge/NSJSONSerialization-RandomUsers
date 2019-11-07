81f286de24373cc9f889ca1b441c2add36a43332
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-07 16:40:08 -0700

    Add initial Xcode project

---
+++ RandomUsers-Objective-C/RandomUsers-Objective-C/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RandomUsers-Objective-C
+//
+//  Created by Mitchell Budge on 11/7/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ RandomUsers-Objective-C/RandomUsers-Objective-C/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  RandomUsers-Objective-C
+//
+//  Created by Mitchell Budge on 11/7/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
